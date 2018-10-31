55c5e5ed5ad6357384c82f3f79c2b11a7de783fd
Junsuk Oh <a811219@gmail.com>
2018-10-31 10:15:39 +0900

    kotlin 1.3 업데이트, 코루틴 테스트

---
+++ app/src/main/java/com/example/koinsample/coroutine/ScpoedAppActivity.kt
@@ -0,0 +1,4 @@
+package com.example.koinsample.coroutine
+
+class ScpoedAppActivity {
+}
--- app/src/main/java/com/example/koinsample/main/MainActivity.kt
+++ app/src/main/java/com/example/koinsample/main/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.example.koinsample.main
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.koinsample.R
+import com.example.koinsample.coroutine.ScopedAppActivity
 import com.example.koinsample.databinding.ActivityMainBinding
 import com.example.koinsample.repository.Controller
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
 import org.jetbrains.anko.toast
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity(), AnkoLogger {
+class MainActivity : ScopedAppActivity(), AnkoLogger {
 
     private val controller: Controller by inject()
     private val myViewModel: MainViewModel by viewModel()
 
     private val binding: ActivityMainBinding by lazy {
-        DataBindingUtil.setContentView<ActivityMainBinding>(this@MainActivity,
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this@MainActivity,
             R.layout.activity_main
         )
     }
@@ -29,6 +32,9 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
 
         if (savedInstanceState == null) {
             toast(controller.service.name)
+
+            asyncShowCountDown()
+            launch { showIOData() }
         }
 
         binding.viewModel = myViewModel
@@ -41,6 +47,31 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
             myViewModel.getTodo(1) { todo ->
                 toast(todo.toString())
             }
+        }
+    }
+
+    private fun asyncShowCountDown() = launch(Dispatchers.Main) {
+        for (i in 10 downTo 1) {
+            countDownTextView.text = "카운트다운 $i ..."
+            delay(500)
+        }
+        countDownTextView.text = "Done!"
+    }
+
+    private suspend fun showIOData() {
+        val deferred = async(Dispatchers.IO) {
+            // impl
+            repeat(15) {
+                delay(500)
+                debug { "background 처리중..." }
+            }
+            100
+        }
+        withContext(Dispatchers.Main) {
+            val data = deferred.await()
+            // Show data in UI
+
+            countDownTextView.text = "showIOData : $data"
         }
     }
 }
