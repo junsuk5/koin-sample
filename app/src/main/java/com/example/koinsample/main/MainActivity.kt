c716d05f7fe4b1a909e1aecaad711f8db16ca725
Junsuk Oh <a811219@gmail.com>
2018-10-31 23:04:19 +0900

    RecyclerView + DataBinding 샘플 추가

--- app/src/main/java/com/example/koinsample/main/MainActivity.kt
+++ app/src/main/java/com/example/koinsample/main/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.koinsample.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.koinsample.R
 import com.example.koinsample.coroutine.ScopedAppActivity
 import com.example.koinsample.databinding.ActivityMainBinding
+import com.example.koinsample.databinding.ItemRecyclerBinding
+import com.example.koinsample.model.DummyData
 import com.example.koinsample.repository.Controller
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
@@ -21,8 +27,8 @@ class MainActivity : ScopedAppActivity() {
 
     private val binding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(
-            this@MainActivity,
-            R.layout.activity_main
+                this@MainActivity,
+                R.layout.activity_main
         )
     }
 
@@ -45,6 +51,34 @@ class MainActivity : ScopedAppActivity() {
         retrofitTestButton.setOnClickListener {
             myViewModel.getTodo(1) { todo ->
                 toast(todo.toString())
+            }
+        }
+
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = MyAdapter(listOf(DummyData(1), DummyData(2), DummyData(3))) { data, pos ->
+                toast("$data : $pos")
+            }
+        }
+    }
+
+    class MyAdapter(private val myDataset: List<DummyData>,
+                    val onItemClickListener: (data: DummyData, pos: Int) -> Unit) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+        class MyViewHolder(val binding: ItemRecyclerBinding) : RecyclerView.ViewHolder(binding.root)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_recycler, parent, false)
+            return MyViewHolder(ItemRecyclerBinding.bind(view))
+        }
+
+        override fun getItemCount() = myDataset.size
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.binding.data = myDataset[position]
+            holder.binding.root.setOnClickListener {
+                onItemClickListener.invoke(myDataset[position], position)
             }
         }
     }
--- app/src/main/java/com/example/koinsample/model/Models.kt
+++ app/src/main/java/com/example/koinsample/model/Models.kt
@@ -1,3 +1,5 @@
 package com.example.koinsample.model
 
 data class Todo(val userId: Int, val id: Int, val title: String, val completed: Boolean)
+
+data class DummyData(val num: Int)
